0103efd38c254d4b36a2f9fc2b340662892d6b3e
thedzy <thedzy@hotmail.com>
2018-09-23 20:58:18 +0200

    Create user_must_choose.sh

---
+++ user_must_choose.sh
@@ -0,0 +1,31 @@
+# Must choose two examples
+
+tput reset
+
+
+# Option 1
+OPT=X
+tput sc
+while [ "${OPT/[^1-4]/X}" == "X" ] || [ ! -n "$OPT" ]; do
+	echo "1. Do nothing"
+	echo "2. Do something"
+	echo "3. Do anything"
+	echo "4. Do or do not, there is no try"
+	read -p "Choose? " -s -n 1 OPT
+	tput el1;tput rc
+done
+
+echo "\n\n\n"
+echo "You choose: $OPT\n"
+
+# Option 2
+tput sc
+while [ ! "$OPT" == "y" ] && [ ! "$OPT" == "n" ]; do
+	read -p "Choose? (y/n) " -s -n 1 OPT
+	OPT=$(echo "$OPT" | tr [:upper:] [:lower:] )
+	tput el1;tput rc
+done
+
+echo "You choose: $OPT"
+
+exit 0
