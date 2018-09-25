088336ce109ddf15c90f980caa34db6e93f1daef
thedzy <thedzy@hotmail.com>
2018-09-25 19:45:29 +0200

    Update columns.sh

--- Functions/Columns/columns.sh
+++ Functions/Columns/columns.sh
@@ -1,9 +1,28 @@
+#!/bin/sh
+#
+# SCRIPT:	columns.sh
+# AUTHOR:	thedzy <thedzy@hotmail.com>
+# DATE:		2018-09-24
+# REV:		1.0
+#
+# PLATFORM: MacOS
+#
+# PURPOSE:
+#	Format data into columns
+#
+# FUNCTIONS: 
+#	columndata()
+# PARAMETERS: 
+#	$1 Cell data
+#	$2 Cell width
+#	$3 Alignment (left, right center)
+#
+
 columndata() {
 	local CDATA="$1"
 	local WIDTH=$(( ${2} - 3 ))
 	local ALIGN=$3
-	
-	
+
 	local LEFT RIGHT
 	
 	[ $WIDTH -le 0 ] && echo " |" && exit
