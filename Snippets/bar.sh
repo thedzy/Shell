68730857d7f470f82fecd4853a152f3a2be4e769
NerdyPepper <akshayoppiliappan@gmail.com>
2018-10-02 10:48:04 +0530

    Add a progress bar Snippet

---
+++ Snippets/bar.sh
@@ -0,0 +1,18 @@
+bar() {
+    # Usage: bar 1 10
+    #            ^----- Elapsed Percentage (0-100).
+    #               ^-- Total length in chars.
+    ((elapsed=$1*$2/100))
+
+    # Create the bar with spaces.
+    printf -v prog  "%${elapsed}s"
+    printf -v total "%$(($2-elapsed))s"
+
+    printf '%s\r' "[${prog// /-}${total}]"
+}
+
+# Usage
+
+for (( i=0; i<=100; i++ )); do
+    bar "$i" "10"
+done
